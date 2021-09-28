2079c936059b8aa0772b2d7d4686cdae20619b53
William Nogueira <will.ribeiro@live.com>
2021-09-28 15:50:25 -0300

    Login e Dashboard - Componente Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
 import { Header, } from "../components/Header";
+import { Flex } from "@chakra-ui/react"
+import { Sidebar } from "../components/Form/Sidebar";
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" height="100vh" >
+      <Header />
+
+      <Flex width="100%" marginY="6" maxWidth={1480} MarginX="auto" PaddingX="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
