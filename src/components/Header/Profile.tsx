eff57fa1a3ab7ead16481c9e446b37100b13f602
William Nogueira <will.ribeiro@live.com>
2021-10-04 16:03:24 -0300

    Responsividade - Header Responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar} from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true}: ProfileProps) {
   return (
     <Flex align="center">
-      <Box marginRight="4" textAlign="right">
+      { showProfileData && (
+        <Box marginRight="4" textAlign="right">
         <Text>william</Text>
         <Text color="gray.300" fontSize="small">
           will.ribeiro@live.com
         </Text>
       </Box>
+      )}
 
       <Avatar size="md" name="William" src="https://github.com/willnogueyra.png" />
     </Flex>
