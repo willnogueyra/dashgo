1fe06c6408fb4335357835b2b33128255f68c8fc
William Nogueira <will.ribeiro@live.com>
2021-10-05 16:26:22 -0300

    Formulários e validação - Form de autenticação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import { Flex, Button, Stack } from "@chakra-ui/react"
+import { SubmitHandler, useForm } from "react-hook-form"
 import { Input } from "../components/Form/Input"
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values)
+  }
+
   return (
     <Flex
       width="100vw"
       height="100vh"
       alignItems="center"
-      justifyContent="center"
+      justify="center"
     >
       <Flex 
         as="form" 
@@ -17,14 +31,20 @@ export default function SignIn() {
         padding="8" // 2rem / 32px
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail"/>
-
-          <Input name="password" type="password" label="Senha"></Input>
+          <Input name="email" type="email" label="E-mail" {...register('email')}/>
+          <Input name="password" type="password" label="Senha" {...register('password')}/>
         </Stack>
 
-        <Button type="submit" marginTop="6" colorScheme="pink" size="lg"> 
+        <Button 
+          type="submit" 
+          marginTop="6" 
+          colorScheme="pink" 
+          size="lg"
+          isLoading={formState.isSubmitting}
+        > 
           Entrar
         </Button>
         
