92aa986cd641108b57a484c5b7a3e2ea37046699
William Nogueira <will.ribeiro@live.com>
2021-10-06 17:29:30 -0300

    Data fetching e cache local - Construindo aplicação - Configurando axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,11 +6,13 @@ import { Pagination } from "../../components/Pagination";
 import Link from "next/link"
 import React, { useEffect } from "react";
 import { useQuery } from "react-query";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading, isFetching ,error } = useQuery('users', async () => {
-    const response = await fetch('https://localhost:3000/api/users')
-    const data = await response.json()
+    const { data } = await api.get('/users')
+
+
 
     const users = data.users.map(user => {
       return {
