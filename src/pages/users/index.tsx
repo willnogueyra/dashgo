f8b1ab50907f10d320b3429cb96c14701999f3bb
William Nogueira <will.ribeiro@live.com>
2021-10-06 17:43:23 -0300

    Data fetching e cache local - Construindo aplicação - Criando hook de listagem

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,36 +1,14 @@
 import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Td, Checkbox, Tbody, Text, useBreakpointValue, Spinner } from "@chakra-ui/react"
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 import { Sidebar } from "../../components/Sidebar";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link"
-import React, { useEffect } from "react";
-import { useQuery } from "react-query";
-import { api } from "../../services/api";
+import React from "react";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching ,error } = useQuery('users', async () => {
-    const { data } = await api.get('/users')
-
-
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      };
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 segundos
-  })
+  const { data, isLoading, isFetching, error } = useUsers();
 
 
   const isWideVersion = useBreakpointValue({
